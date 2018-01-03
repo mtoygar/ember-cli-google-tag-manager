8520dd7080dc59c0048cc70533cb08a56c0d4b03
Soulou <leo@unbekandt.eu>
2018-01-03 15:04:16 +0100

    Do not inject service if appId is not present

--- addon/initializers/gtm.js
+++ addon/initializers/gtm.js
@@ -1,4 +1,13 @@
+import environment from "../../config/environment"
+
 export function initialize(container /*, application */) {
+  // Do not inject anythin in the router if appId is not present
+  config = environment.googleTagManager
+  appId = config && config.appId
+  if (!appId) {
+    return
+  }
+
   var router = container.lookup('router:main');
   var gtm = container.lookup('service:gtm')
 
