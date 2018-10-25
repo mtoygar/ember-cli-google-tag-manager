157422965097538dc93f9f36edb305b4e3e12235
demirhanaydin <demirhanaydin@gmail.com>
2018-10-25 15:44:53 +0300

    fix ember env variables resolve

--- addon/instance-initializers/gtm.js
+++ addon/instance-initializers/gtm.js
@@ -1,15 +1,14 @@
 export function initialize(application) {
   // Do not inject anythin in the router if appId is not present
-  var container = application.container;
-  var environment = container.lookupFactory('config:environment') || {};
+  var environment = application.resolveRegistration('config:environment') || {};
   var config = environment.googleTagManager;
   var appId = config && config.appId;
   if (!appId) {
     return;
   }
 
-  var router = container.lookup('router:main');
-  var gtm = container.lookup('service:gtm');
+  var router = application.lookup('router:main');
+  var gtm = application.lookup('service:gtm');
 
   router.on('didTransition', function () {
     gtm.trackPageView(this.get('url'));
