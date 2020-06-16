a784388f97f32f264f83a8c9b1314db2078a6db2
Murat Toygar <toygar-murat@hotmail.com>
2020-06-16 15:18:33 +0300

    further add ember 3.10 related updates

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -1,17 +1,12 @@
-import Ember from 'ember';
 import Application from '@ember/application';
-import Resolver from 'ember/resolver';
-import loadInitializers from 'ember/load-initializers';
+import Resolver from './resolver';
+import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
-var App;
-
-Ember.MODEL_FACTORY_INJECTIONS = true;
-
-App = Application.extend({
+const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver
+  Resolver
 });
 
 loadInitializers(App, config.modulePrefix);
