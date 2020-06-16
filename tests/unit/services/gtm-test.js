8fb53191534bc5fb70e0830e5dd3d8a268d913d3
Murat Toygar <toygar-murat@hotmail.com>
2020-06-16 15:35:46 +0300

    fix deprecations for moduleFor

--- tests/unit/services/gtm-test.js
+++ tests/unit/services/gtm-test.js
@@ -1,32 +1,31 @@
-import { moduleFor, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 
-moduleFor('service:gtm', 'Unit | Service | gtm', {
-  integration: true
-  // Specify the other units that are required for this test.
-  // needs: ['service:foo']
-});
+module('Unit | Service | Flash', function(hooks) {
+  setupTest(hooks);
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  var service = this.subject();
-  assert.ok(service);
-});
+  // Replace this with your real tests.
+  test('it exists', function(assert) {
+    let service = this.owner.lookup('service:gtm');
+    assert.ok(service);
+  });
 
-test('it should add common fields in all payloads', function(assert) {
-  var service = this.subject();
-  window.dataLayer = [];
-  service.addCommonField("userId", "1");
+  test('it should add common fields in all payloads', function(assert) {
+    let service = this.owner.lookup('service:gtm');
+    window.dataLayer = [];
+    service.addCommonField("userId", "1");
 
-  service.trackEvent();
-  assert.equal(window.dataLayer.length, 1);
-  var e = window.dataLayer[0];
-  assert.equal(e.userId, "1");
+    service.trackEvent();
+    assert.equal(window.dataLayer.length, 1);
+    let e = window.dataLayer[0];
+    assert.equal(e.userId, "1");
 
-  window.dataLayer = [];
-  service.removeCommonField("userId");
+    window.dataLayer = [];
+    service.removeCommonField("userId");
 
-  service.trackEvent();
-  assert.equal(window.dataLayer.length, 1);
-  e = window.dataLayer[0];
-  assert.equal(e.userId, undefined);
+    service.trackEvent();
+    assert.equal(window.dataLayer.length, 1);
+    e = window.dataLayer[0];
+    assert.equal(e.userId, undefined);
+  });
 });
